faaa1639260c5b47055ec7512eb93928dfd04f7a
nampc <namphamc99@gmail.com>
2023-02-24 23:05:09 +0700

    Update Transfer logic and allow Approval

--- lib.rs
+++ lib.rs
@@ -9,7 +9,7 @@ mod erc20 {
     pub struct Erc20 {
         total_supply: Balance,
         balance_map: Mapping<AccountId, Balance>,
-        allowances: Mapping<(AccountId, AccountId), Balance>
+        allowances: Mapping<(AccountId, AccountId), Balance> // key is (spender, owner), Balance is the maximum amount allowed
     }
 
     #[ink(event)]
@@ -19,9 +19,16 @@ mod erc20 {
         value: Balance
     }
 
+    #[ink(event)]
+    pub struct Approval {
+        owner: AccountId,
+        spender: AccountId,
+        max_spending: Balance
+    }
+
     // enum to handle emitting and returning error
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
-    // #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         InsufficientBalance,
         InsuffcientAllowance
@@ -66,21 +73,68 @@ mod erc20 {
         #[ink(message)]
         pub fn transfer (&mut self, to: AccountId, value: Balance) -> Result<(), Error> {
             let from = self.env().caller();
-            // Firstly, check balance of From account
-            let from_balance = self.balance_of(from);
 
-            if from_balance < value {
-                // If insufficient balance, throws new error and emits event
-                return Err(Error::InsufficientBalance)
-            }
+            self.transfer_from_to(from, to, value)
+        }
 
-            self.balance_map.insert(from, &(from_balance - value));
-
-            let current_to_balance = self.balance_of(to);
+        fn check_transfer (&self, from: &AccountId, value: Balance) -> Result<(), Error> {
+            let from_balance = self.balance_map.get(from).unwrap_or(0);
             
-            self.balance_map.insert(to, &(current_to_balance + value));
+            if from_balance < value {
+                Err(Error::InsufficientBalance)
+            } else {
+                Ok(())
+            }
+        }
+
+        fn transfer_from_to (&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<(), Error> {
+            // Firstly, check balance of From account
+            let check_transfer = self.check_transfer(&from, value);
+
+            match check_transfer {
+                Ok(()) => {
+                    let from_balance = self.balance_of(from);
+                    let current_to_balance = self.balance_of(to);
+
+                    self.balance_map.insert(from, &(from_balance - value));
+                    self.balance_map.insert(to, &(current_to_balance + value));
+    
+                    Ok(())
+                }
+
+                Err(Error::InsufficientBalance) => {
+                    Err(Error::InsufficientBalance)
+                }
+
+                Err(Error::InsuffcientAllowance) => {
+                    Err(Error::InsuffcientAllowance)
+                }
+            }
+        }
+
+        #[ink(message)]
+        pub fn approve (&mut self, spender: AccountId, max_spending: Balance) -> Result<(), Error> {
+            let owner = self.env().caller();
+            self.allowances.insert((&spender, &owner), &max_spending);
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                max_spending
+            });
 
             Ok(())
+        }
+
+        pub fn transfer_with_allowance (&mut self, owner: AccountId, to: AccountId, value: Balance) -> Result<(), Error> {
+            let caller = self.env().caller();
+
+            let allowance_of_caller = self.allowances.get((&caller, &owner)).unwrap_or_default();
+
+            if allowance_of_caller > value {
+                return Err(Error::InsuffcientAllowance);
+            } else {
+                return self.transfer_from_to(owner, to, value);
+            }
         }
     }
 }
